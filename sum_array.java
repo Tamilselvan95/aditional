f0e0ae0b37a51f52ab821f974420e45f41d942cb
Tamilselvan <tamilselvanvkpm@gmail.com>
2016-07-30 13:39:38 +0530

    Create sum_array.java

---
+++ sum_array.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class sum_array{
+
+     public static void main(String []args){
+        int[] a=new int[1];
+      int[] b=new int[1];
+      Scanner s=new Scanner(System.in);
+      a[0]=s.nextInt();
+      b[0]=s.nextInt();
+      int c=a[0]+b[0];
+      System.out.println(c);
+     }
+}
